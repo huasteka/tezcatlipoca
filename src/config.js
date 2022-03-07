269308da0cb9119ee24dc1607612b2b53cc0f057
Murilo Costa <murilo.nunes.jose@gmail.com>
2022-03-08 00:06:32 +0100

    GH-11 Create configuration file for production

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  zacatzontli: 'https://zacatzontli.herokuapp.com',
+  tepoztecatl: 'https://tepoztecatl.herokuapp.com',
+  yacatecuhtli: 'https://yacatecuhtli.herokuapp.com',
+  huitzilopochtli: 'https://huitzilopochtli.herokuapp.com',
+};
